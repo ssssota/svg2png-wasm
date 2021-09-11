8bc396fef537a1958dbfb93f6e8f49ad74a4bcbf
Sotaro Tommykawa <tommysouta@gmail.com>
2021-09-11 21:10:20 +0900

    👷 Add esm build

--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,31 @@ import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
 import { terser } from 'rollup-plugin-terser';
 
-export default defineConfig({
-  input: 'lib/cjs/index.ts',
-  output: {
-    dir: 'cjs',
-    format: 'cjs',
+export default defineConfig([
+  {
+    input: 'lib/cjs/index.ts',
+    output: {
+      dir: 'cjs',
+      format: 'cjs',
+    },
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      typescript({ rootDir: 'lib/cjs', outDir: 'cjs' }),
+      terser(),
+    ],
   },
-  plugins: [
-    commonjs(),
-    nodeResolve(),
-    typescript({ rootDir: 'lib/cjs', outDir: 'cjs' }),
-    terser(),
-  ],
-});
+  {
+    input: 'lib/cjs/index.ts',
+    output: {
+      dir: 'esm',
+      format: 'esm',
+    },
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      typescript({ rootDir: 'lib/cjs', outDir: 'esm' }),
+      terser(),
+    ],
+  },
+]);
