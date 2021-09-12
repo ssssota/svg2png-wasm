1732c2615f8028c788d23b5ee544e7a79eccbaab
Sotaro Tommykawa <tommysouta@gmail.com>
2021-09-12 17:45:22 +0900

    ✨ Add default font

--- src/converter.rs
+++ src/converter.rs
@@ -7,6 +7,11 @@ use wasm_bindgen::prelude::*;
 #[derive(Clone)]
 pub struct Converter {
     fonts: Vec<Vec<u8>>,
+    serif_family: Option<String>,
+    sans_serif_family: Option<String>,
+    cursive_family: Option<String>,
+    fantasy_family: Option<String>,
+    monospace_family: Option<String>,
 }
 
 #[wasm_bindgen]
@@ -24,18 +29,25 @@ impl Converter {
         width: Option<f64>,
         height: Option<f64>,
     ) -> Option<Vec<u8>> {
-        let fontdb = load_fonts(&self.fonts);
+        let fontdb = load_fonts(
+            &self.fonts,
+            self.serif_family.as_ref().map(|s| s.as_str()),
+            self.sans_serif_family.as_ref().map(|s| s.as_str()),
+            self.cursive_family.as_ref().map(|s| s.as_str()),
+            self.fantasy_family.as_ref().map(|s| s.as_str()),
+            self.monospace_family.as_ref().map(|s| s.as_str()),
+        );
         let svg_options = OptionsRef {
             resources_dir: None,
-            dpi: 192.0,
-            font_family: "",
-            font_size: 0.0,
+            dpi: 96.0,
+            font_family: "sans-serif",
+            font_size: 12.0,
             languages: &["en".to_string()],
             shape_rendering: usvg::ShapeRendering::GeometricPrecision,
             text_rendering: usvg::TextRendering::OptimizeLegibility,
             image_rendering: usvg::ImageRendering::OptimizeQuality,
             keep_named_groups: false,
-            default_size: Size::new(width.unwrap_or(1.0), height.unwrap_or(1.0)).unwrap(),
+            default_size: Size::new(width.unwrap_or(100.0), height.unwrap_or(100.0)).unwrap(),
             fontdb: &fontdb,
         };
         let scale = scale.unwrap_or(1.0);
@@ -53,14 +65,39 @@ impl Converter {
 }
 
 #[wasm_bindgen(js_name = createConverter)]
-pub fn create_converter() -> Converter {
-    Converter { fonts: Vec::new() }
+pub fn create_converter(
+    default_serif_family: Option<String>,
+    default_sans_serif_family: Option<String>,
+    default_cursive_family: Option<String>,
+    default_fantasy_family: Option<String>,
+    default_monospace_family: Option<String>,
+) -> Converter {
+    Converter {
+        fonts: Vec::new(),
+        serif_family: default_serif_family,
+        sans_serif_family: default_sans_serif_family,
+        cursive_family: default_cursive_family,
+        fantasy_family: default_fantasy_family,
+        monospace_family: default_monospace_family,
+    }
 }
 
-pub fn load_fonts(fonts: &[Vec<u8>]) -> Database {
+pub fn load_fonts(
+    fonts: &[Vec<u8>],
+    default_serif_family: Option<&str>,
+    default_sans_serif_family: Option<&str>,
+    default_cursive_family: Option<&str>,
+    default_fantasy_family: Option<&str>,
+    default_monospace_family: Option<&str>,
+) -> Database {
     let mut db = Database::new();
     for font in fonts {
         db.load_font_data(font.to_vec());
     }
+    default_serif_family.map(|f| db.set_serif_family(f.to_string()));
+    default_sans_serif_family.map(|f| db.set_sans_serif_family(f.to_string()));
+    default_cursive_family.map(|f| db.set_cursive_family(f.to_string()));
+    default_fantasy_family.map(|f| db.set_fantasy_family(f.to_string()));
+    default_monospace_family.map(|f| db.set_monospace_family(f.to_string()));
     db
 }
