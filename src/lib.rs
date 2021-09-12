f1d607e89f9e4907da57ed4b167b84ff3923ef33
Sotaro Tommykawa <tommysouta@gmail.com>
2021-09-12 17:14:58 +0900

    ✨ Add error panic hook

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
 pub mod converter;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn initialize() {
+  #[cfg(feature = "console_error_panic_hook")]
+  console_error_panic_hook::set_once();
+}
