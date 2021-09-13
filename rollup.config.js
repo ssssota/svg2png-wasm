83197032eb7ccff031c46715ec0f40db6214dee7
Sotaro Tommykawa <tommysouta@gmail.com>
2021-09-13 16:02:25 +0900

    🏗️ Fix build architecture

--- rollup.config.js
+++ rollup.config.js
@@ -2,50 +2,72 @@ import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 import { defineConfig } from 'rollup';
+
 const pkg = require('./package.json');
+
+const customReplacer = replace({
+  preventAssignment: true,
+  values: {
+    'new TextDecoder': `new (typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder)`,
+    'new TextEncoder': `new (typeof TextEncoder === 'undefined' ? (0, module.require)('util').TextEncoder : TextEncoder)`,
+  },
+});
 
 export default defineConfig([
   {
-    input: 'lib/cjs/index.ts',
-    output: {
-      dir: 'cjs',
-      format: 'cjs',
-    },
-    plugins: [
-      commonjs(),
-      nodeResolve(),
-      typescript({ rootDir: 'lib/cjs', outDir: 'cjs' }),
+    input: 'lib/main.ts',
+    output: [
+      {
+        file: 'main/index.js',
+        format: 'esm',
+      },
+      {
+        file: 'main/index.cjs',
+        format: 'cjs',
+      },
     ],
+    plugins: [commonjs(), nodeResolve(), customReplacer, typescript()],
   },
   {
-    input: 'lib/cjs/index.ts',
-    output: {
-      dir: 'esm',
-      format: 'esm',
-    },
-    plugins: [
-      commonjs(),
-      nodeResolve(),
-      typescript({ rootDir: 'lib/cjs', outDir: 'esm' }),
+    input: 'lib/core.ts',
+    output: [
+      {
+        file: 'core/index.cjs',
+        format: 'cjs',
+      },
+      {
+        file: 'umd/index.js',
+        format: 'umd',
+        name: 'SVG2PNG_CORE',
+      },
+      {
+        file: 'core/index.js',
+        format: 'esm',
+      },
     ],
+    plugins: [commonjs(), nodeResolve(), customReplacer, typescript()],
   },
   {
-    input: 'lib/umd/index.ts',
+    input: 'lib/unpkg.ts',
     output: {
-      dir: 'umd',
+      file: 'unpkg/index.js',
       format: 'umd',
       name: 'SVG2PNG',
     },
     plugins: [
       replace({
-        'process.env.SVG2PNG_WASM_URL': JSON.stringify(
-          `https://unpkg.com/svg2png-wasm@${pkg.version}/umd/svg2png_wasm_bg.wasm`,
+        preventAssignment: true,
+        'process.env.UNPKG_WASM_PATH': JSON.stringify(
+          `https://unpkg.com/svg2png-wasm@${pkg.version}/svg2png_wasm_bg.wasm`,
         ),
       }),
       commonjs(),
       nodeResolve(),
-      typescript({ rootDir: 'lib/umd', outDir: 'umd' }),
+      customReplacer,
+      typescript(),
+      terser(),
     ],
   },
 ]);
