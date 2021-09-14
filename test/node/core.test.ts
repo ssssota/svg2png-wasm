c5568cc914859841c63d07c515b2b6057e3a43da
ssssota <tommysouta@gmail.com>
2021-09-14 12:36:26 +0900

    🔇 Remove extra logs

--- test/node/core.test.ts
+++ test/node/core.test.ts
@@ -6,7 +6,6 @@ import { join } from 'path';
 it('invalid wasm', (done) => {
   const wasm = join(__dirname, '../../LICENSE');
   const svg2png = createSvg2png(readFileSync(wasm));
-  console.log({ wasm, svg2png });
   svg2png(`
     <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
       <circle cx="5" cy="5" r="5" />
