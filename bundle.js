54434ee4da0425e30dd58371bc7597b4c1e10db6
Sotaro Tommykawa <tommysouta@gmail.com>
2021-10-12 12:11:38 +0900

    🚚 move dist paths

--- bundle.js
+++ bundle.js
@@ -1,80 +1,28 @@
 // @ts-check
 import { buildSync } from 'esbuild';
-import { readFileSync } from 'node:fs';
-import { resolve, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-/** @type {{version:string}} */
-const { version } = JSON.parse(
-  readFileSync(
-    // @ts-ignore
-    resolve(dirname(fileURLToPath(import.meta.url)), 'package.json'),
-    'utf8',
-  ),
-);
 
 /** @type {import('esbuild').BuildOptions} */
 const commonOptions = {
   bundle: true,
   logLevel: 'error',
-};
-
-/** @type {import('esbuild').BuildOptions} */
-const coreOptions = {
-  ...commonOptions,
-  entryPoints: ['lib/core.ts'],
+  entryPoints: ['lib/index.ts'],
   define: { 'import.meta.url': 'undefined' },
 };
 
-/** @type {import('esbuild').BuildOptions} */
-const nodeOptions = {
+buildSync({
   ...commonOptions,
-  entryPoints: ['lib/main.ts'],
-  platform: 'node',
-};
-
-// core modules
-buildSync({
-  ...coreOptions,
   format: 'cjs',
-  outfile: 'core/index.cjs',
+  outfile: 'dist/index.cjs',
 });
 buildSync({
-  ...coreOptions,
+  ...commonOptions,
   format: 'esm',
-  outfile: 'core/index.js',
+  outfile: 'dist/index.mjs',
 });
 buildSync({
-  ...coreOptions,
+  ...commonOptions,
   format: 'iife',
   minify: true,
-  globalName: 'SVG2PNG_CORE',
+  globalName: 'svg2pngWasm',
   outfile: 'umd/index.js',
-});
-
-// node modules
-buildSync({
-  ...nodeOptions,
-  format: 'cjs',
-  outfile: 'main/index.cjs',
-});
-buildSync({
-  ...nodeOptions,
-  format: 'esm',
-  outfile: 'main/index.js',
-});
-
-// unpkg modules
-buildSync({
-  ...commonOptions,
-  entryPoints: ['lib/unpkg.ts'],
-  format: 'iife',
-  globalName: 'SVG2PNG',
-  minify: true,
-  outfile: 'unpkg/index.js',
-  define: {
-    'process.env.UNPKG_WASM_PATH': JSON.stringify(
-      `https://unpkg.com/svg2png-wasm@${version}/svg2png_wasm_bg.wasm`,
-    ),
-  },
 });
