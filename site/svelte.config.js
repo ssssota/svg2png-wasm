d5d170bbda6311343a027cccfa00fed7808faee1
Sotaro Tommykawa <tommysouta@gmail.com>
2021-10-13 01:52:07 +0900

    🚚 move base path for dev mode

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -2,21 +2,23 @@ import preprocess from 'svelte-preprocess';
 import adapt from '@sveltejs/adapter-static';
 import { optimizeImports, optimizeCss } from 'carbon-preprocess-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [optimizeImports(), preprocess({ postcss: true })],
+	preprocess: [preprocess({ postcss: true }), optimizeImports()],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: adapt(),
 		paths: {
-			base: '/svg2png-wasm'
+			base: dev ? undefined : '/svg2png-wasm'
 		},
 		vite: {
-			plugins: [process.env.NODE_ENV === 'production' && optimizeCss()]
+			plugins: [!dev && optimizeCss()]
 		}
 	}
 };
