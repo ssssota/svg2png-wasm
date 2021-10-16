d4ba905c3d4382978a2bac20e8a2f68097041578
Sotaro Tommykawa <tommysouta@gmail.com>
2021-10-17 01:18:05 +0900

    ✅ add test for size checks

--- test/node/index.test.ts
+++ test/node/index.test.ts
@@ -5,8 +5,69 @@ beforeAll(async () => {
   await initialize(readFileSync('./svg2png_wasm_bg.wasm'));
 });
 
-it('invalid svg', async () => {
-  await expect(svg2png('')).rejects.toThrow(
-    'SVG data parsing failed cause the document does not have a root node',
+it('promise should be rejected when initialize twice', async () => {
+  await expect(initialize('')).rejects.toThrow(
+    'Already initialized. The `initialize` function can be used only once.',
   );
+});
+
+describe('svg2png', () => {
+  it('should throw because invalid svg (blank string)', async () => {
+    await expect(svg2png('')).rejects.toThrow(
+      'SVG data parsing failed cause the document does not have a root node',
+    );
+  });
+
+  it('should throw because invalid svg (missing width and height)', async () => {
+    await expect(
+      svg2png('<svg xmlns="http://www.w3.org/2000/svg"></svg>'),
+    ).rejects.toThrow('Invalid width or height');
+  });
+
+  it('should convert because specify width and height', async () => {
+    await expect(
+      svg2png('<svg xmlns="http://www.w3.org/2000/svg"></svg>', {
+        width: 1,
+        height: 1,
+      }),
+    ).resolves.toBeInstanceOf(Uint8Array);
+  });
+
+  it('shoud convert with minimal svg', async () => {
+    await expect(
+      svg2png(
+        '<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg"></svg>',
+      ),
+    ).resolves.toBeInstanceOf(Uint8Array);
+  });
+
+  it('shoud throw because invalid width', async () => {
+    await expect(
+      svg2png(
+        '<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg"></svg>',
+        { width: 0 },
+      ),
+    ).rejects.toThrow('Invalid width or height');
+  });
+
+  it('shoud throw because invalid scale', async () => {
+    await expect(
+      svg2png(
+        '<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg"></svg>',
+        { scale: 0 },
+      ),
+    ).rejects.toThrow('Invalid width or height');
+  });
+
+  it('shoud convert with specified width because width have priority', async () => {
+    const expected = await svg2png(
+      '<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg"></svg>',
+      { width: 10 },
+    );
+    const actual = await svg2png(
+      '<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg"></svg>',
+      { width: 10, scale: 100 },
+    );
+    expect(actual).toStrictEqual(expected);
+  });
 });
