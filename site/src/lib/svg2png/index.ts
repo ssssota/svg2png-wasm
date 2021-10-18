d7593362a9b924aabe9f6acdcf4f7429e532643f
Sotaro Tommykawa <tommysouta@gmail.com>
2021-10-18 22:00:32 +0900

    🚨 fix for svg2png type

--- site/src/lib/svg2png/index.ts
+++ site/src/lib/svg2png/index.ts
@@ -2,7 +2,9 @@ import type { ConverterOptions, ConvertOptions, Svg2png } from 'svg2png-wasm';
 import type { Svg2pngResponse } from './worker';
 import Svg2pngWorker from './worker?worker';
 
-export const createSvg2png = (options?: ConverterOptions): Svg2png => {
+export const createSvg2png = (
+	options?: ConverterOptions
+): Omit<Svg2png, 'getLoadedFontFamilies'> => {
 	const svg2pngWorker = new Svg2pngWorker();
 	svg2pngWorker.postMessage({ type: 'initialize', options });
 
