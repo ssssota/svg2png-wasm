d6e30c8becfba6bc23ea191f653449e9a690ca38
ssssota <ssssota@users.noreply.github.com>
2023-06-10 06:23:27 +0000

    deploy: 39017112dffb6cca3f545fe285a31f171536c4d7

---
+++ _app/immutable/chunks/paths.a58474ef.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_thxjx)==null?void 0:s.base)??"/svg2png-wasm";var t;const e=((t=globalThis.__sveltekit_thxjx)==null?void 0:t.assets)??a;export{e as a,a as b};
