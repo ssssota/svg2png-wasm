b09b7a7c7c243a6d797f86a0159bd5b5afe0b271
ssssota <tommysouta@gmail.com>
2023-10-07 14:59:19 +0900

    chore: remove benchmark

--- benchmark/src/renderer/types.ts
+++
@@ -1,8 +0,0 @@
-export type Render = (svg: string) => Promise<Uint8Array>;
-
-export type Renderer = {
-  init?: () => Promise<void>;
-  render: Render;
-};
-
-export const createRenderer = (r: Renderer) => r;
